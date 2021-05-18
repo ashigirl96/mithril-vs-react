05e33d734b853b857786477316423b19343314b2
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2021-05-18 18:06:05 +0900

    add grand child to didMount example

--- mithril/src/did_mount.js
+++ mithril/src/did_mount.js
@@ -1,15 +1,69 @@
 import m from "../mithril";
 
+export const GrandChild1 = {
+  controller: function() {
+    console.log("Start GRAND CHILD 1 controller()");
+
+    this.didMount = (elem, isInitialized, context) => {
+      console.log(`Start GRAND CHILD 1 didMount ${elem}`);
+      if (isInitialized) return;
+      this.elem = elem;
+      context.onunload = () => {
+        console.log("Start GRAND CHILD 1 onunload");
+        this.elem = null;
+      }
+    }
+  },
+  view: (ctrl, args) => {
+    console.log("Start GRAND CHILD 1 view()");
+    return (
+      <div config={ctrl.didMount.bind(ctrl)}>I'm grand child</div>
+    )
+  }
+}
+
+export const GrandChild2 = {
+  controller: function() {
+    console.log("Start GRAND CHILD 2 controller()");
+
+    this.didMount = (elem, isInitialized, context) => {
+      console.log(`Start GRAND CHILD 2 didMount ${elem}`);
+      if (isInitialized) return;
+      this.elem = elem;
+      context.onunload = () => {
+        console.log("Start GRAND CHILD 2 onunload");
+        this.elem = null;
+      }
+    }
+  },
+  view: (ctrl, args) => {
+    console.log("Start GRAND CHILD 2 view()");
+    return (
+      <div config={ctrl.didMount.bind(ctrl)}>I'm grand child2</div>
+    )
+  }
+}
+
 export const Child = {
   controller: function() {
     console.log("Start CHILD controller()");
 
-    this.didMount = (elem, isInitialized, context) => {
-      console.log(`Start CHILD didMount ${elem}`);
+    this.didMount1 = (elem, isInitialized, context) => {
+      console.log(`Start CHILD didMount1 ${elem}`);
       if (isInitialized) return;
       this.elem = elem;
       context.onunload = () => {
-        console.log("Start CHILD onunload");
+        console.log("Start CHILD onunload1");
+        this.elem = null;
+      }
+    }
+
+    this.didMount2 = (elem, isInitialized, context) => {
+      console.log(`Start CHILD didMount2 ${elem}`);
+      if (isInitialized) return;
+      this.elem = elem;
+      context.onunload = () => {
+        console.log("Start CHILD onunload2");
         this.elem = null;
       }
     }
@@ -17,7 +71,12 @@ export const Child = {
   view: (ctrl, args) => {
     console.log("Start CHILD view()");
     return (
-      <div config={ctrl.didMount.bind(ctrl)}>I'm child</div>
+      <div>
+        <div config={ctrl.didMount1.bind(ctrl)}> I'm child</div>
+        <GrandChild1 />
+        <GrandChild2 />
+        <div config={ctrl.didMount2.bind(ctrl)}> I'm child</div>
+      </div>
     )
   }
 }
@@ -25,6 +84,7 @@ export const Child = {
 export const DidMount = {
   controller: function() {
     console.log("Start PARENT controller()");
+    this.toggle = false;
 
     this.didMount = (elem, isInitialized, context) => {
       console.log("Start PARENT didMount");
@@ -41,8 +101,16 @@ export const DidMount = {
     console.log("Start PARENT view()");
     return (
       <div>
+        <button onclick={() => {
+          ctrl.toggle = !ctrl.toggle;
+          console.log(ctrl.toggle);
+        }} >
+          Click
+        </button>
         <div config={ctrl.didMount.bind(ctrl)} />
-        <Child />
+        {ctrl.toggle ? (
+          <Child />
+        ): null}
       </div>
     )
   }
