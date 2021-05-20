156ddec450e9381c1cbd46f6f4f9d17512fe5d6d
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2021-05-20 20:17:52 +0900

    Add logging lifecycle method

--- mithril-on-react/src/mithril_app.mjs
+++ mithril-on-react/src/mithril_app.mjs
@@ -1,13 +1,23 @@
 import m from "../mithril.js";
 
 export const HelloMithril = {
-  controller: (args) => {
+  controller: function(args) {
     console.log("Start Mithril controller");
+
+    this.didMount = (elem, isInitialized, context) => {
+      console.log("Start Mithril didMount");
+      if (isInitialized) return;
+      this.elem = elem;
+      context.onunload = () => {
+        console.log("Start Mithril onunload");
+        this.elem = null;
+      }
+    }
   },
   view: (ctrl, args) => {
     console.log("Start Mithril view");
     return (
-      <div>Hello, I'm Mithril</div>
+      <div config={ctrl.didMount.bind(ctrl)}>Hello, I'm Mithril</div>
     );
   }
 }
--- mithril-on-react/src/react_app.js
+++ mithril-on-react/src/react_app.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-export const HelloReact = () => {
-  return (
-    <div>Hello, I'm React</div>
-  )
+export class HelloReact extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Start React controller()");
+  }
+
+  componentDidMount() {
+    console.log("Start React didMount");
+  }
+
+  componentWillUnmount() {
+    console.log("Start React onunload");
+  }
+
+  render() {
+    console.log("Start React view()");
+
+    return (
+      <div>Hello, I'm React</div>
+    );
+  }
 }
