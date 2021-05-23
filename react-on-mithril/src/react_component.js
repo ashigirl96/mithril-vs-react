f711b199b87f4f78d5d5ea69f025fd1275333479
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2021-05-23 17:55:17 +0900

    add lint-stage

--- react-on-mithril/src/react_component.js
+++ react-on-mithril/src/react_component.js
@@ -3,17 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export const ReactComponent = {
-  controller: function(args) {
-    this.callReact = (elem, isInitialized, context) => {
-      ReactDOM.render(React.createElement(args.component, {...args}, []), elem);
-      context.onunload = function () {
-        ReactDOM.unmountComponentAtNode(elem);
-      }
-    }
-  },
-  view: (ctrl) => {
+  view: () => {
     return (
-      <div config={ctrl.callReact.bind(ctrl)} />
+      <div config={
+        (elem, isInitialized, context) => {
+          ReactDOM.render(React.createElement(args.component, {...args}, []), elem);
+          context.onunload = function () {
+            ReactDOM.unmountComponentAtNode(elem);
+          }
+        }
+      } />
     );
   }
 }
